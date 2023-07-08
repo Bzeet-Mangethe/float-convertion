1e977f42ca881b4e88f56a619af09e3ad89d9c80
Bhekani Masinga <bhekani.masinga@nagarro.com>
2023-07-08 18:46:06 +0200

    first commit

---
+++ ass1Float/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass1Float
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a float: ");
+            string input = Console.ReadLine();
+            float result;
+            if (float.TryParse(input, out result))
+            {
+                Console.WriteLine("Parsed float: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+        }
+    }
+}
